c32ef97a47cad879743064a56704125f8717f17c
Benjamin Soyka <37779854+bsoyka@users.noreply.github.com>
2018-04-08 21:20:09 -0500

    Update shortest.js

--- shortest.js
+++ shortest.js
@@ -1,13 +1,11 @@
-<script type="text/javascript">
-  var adfly_id = 19352257;
-  var adfly_advert = 'int';
-  var adfly_protocol = 'http';
-  var adfly_domain = 'go.soyka.ml';
-  var exclude_domains = ['soyka.ml','b.soyka.ml'];
-  var frequency_cap = '5';
-  var frequency_delay = '5';
-  var init_delay = '3';
-  var popunder = true;
-</script>
+var adfly_id = 19352257;
+var adfly_advert = 'int';
+var adfly_protocol = 'http';
+var adfly_domain = 'go.soyka.ml';
+var exclude_domains = ['soyka.ml','b.soyka.ml'];
+var frequency_cap = '5';
+var frequency_delay = '5';
+var init_delay = '3';
+var popunder = true;
 <script src="https://cdn.adf.ly/js/link-converter.js"></script>
 <script src="https://cdn.adf.ly/js/entry.js"></script>

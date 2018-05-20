57d4b8de67d89e10b9ba53b8ba2d604f65ff80a9
Benjamin Soyka <bensoyka@icloud.com>
2018-05-20 10:24:56 -0500

    Changes to password generator

--- passgen/main.js
+++ passgen/main.js
@@ -1,8 +1,27 @@
 function populateForm(outputElement, passwordLength) {
-    var characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()~-_=+[]{}\;:'<>,./?";
+    var numbers = "1234567890";
+    var lowercase = "abcdefghijklmnopqrstuvwxyz";
+    var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var special = "~!@#$%^&*()-_=+[]{}\':;<>,./?";
+    var characters = "";
+    var form = document.getElementById("passGen");
+    var formElements = form.elements;
+    var formatOptions = formElements.format;
+    if (formatOptions[0].checked) {
+        characters += numbers;
+    }
+    if (formatOptions[1].checked) {
+        characters += lowercase;
+    }
+    if (formatOptions[2].checked) {
+        characters += uppercase;
+    }
+    if (formatOptions[3].checked) {
+        characters += special;
+    }
     var output = "";
     for (var i = 0; i < passwordLength; i++) {
-        output+=characters.charAt(Math.floor(Math.random() * characters.length));
+        output += characters.charAt(Math.floor(Math.random() * characters.length));
     }
     outputElement.value = output;
     outputElement.setAttribute("size", output.length + 1);
